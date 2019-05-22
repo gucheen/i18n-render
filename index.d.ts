39b9d9685ed6caffea0874dee71459157cefb924
gucheen <guchengf@gmail.com>
2019-05-22 10:13:41 +0800

    add(tsd)

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+export interface I18nTranslationGroup {
+  [locale: string]: string;
+}
+
+export interface I18nSource {
+  [key: string]: I18nTranslationGroup;
+}
+
+declare class I18nRender {
+  locale: string;
+
+  constructor(options: { source: I18nSource; locale?: string; pageTextSource?: string; fullLangTag?: boolean })
+
+  getLocale(): string;
+
+  setLocale(params: { locale: string; persist?: boolean; rerender?: boolean }): void;
+
+  getText(params: { key: string; locale?: string; fallback?: string }): string;
+
+  render(): void;
+}
